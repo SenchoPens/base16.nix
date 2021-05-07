850d1ba8e9eac5d3ba159c6146e58ba48428114d
Arseniy Chekanov <senya@chekanov.net>
2021-05-08 00:51:11 +0300

    Fix: add flake-utils to properly make the flake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    lib = (import ./.) { lib = nixpkgs.lib; pkgs = nixpkgs.pkgs; };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        lib = (import ./.) { lib = pkgs.lib; inherit pkgs; };
+  });
 }
