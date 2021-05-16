33d099207af353d0f900bcc0c2d493b6cf529a1f
Arseniy Chekanov <senya@chekanov.net>
2021-05-16 23:25:47 +0300

    fix: Rename numberedList -> numbered

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
   getScheme = { base00, base01, base02, base03, base04, base05, base06, base07
     , base08, base09, base0A, base0B, base0C, base0D, base0E, base0F, ... }@numbered:
   rec {
-    numberedList = [ base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F ];
+    numbered = [ base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F ];
 
     inherit numbered;
     numberedHashtag = builtins.mapAttrs (_: v: "#" + v) numbered;
