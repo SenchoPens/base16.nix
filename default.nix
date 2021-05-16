5011c060f1110c0ca47977a405086fc4c7aca1d5
Arseniy Chekanov <senya@chekanov.net>
2021-05-16 23:40:46 +0300

    fix: Remove redundant `inherit numbered`

--- default.nix
+++ default.nix
@@ -5,8 +5,6 @@ let
     , base08, base09, base0A, base0B, base0C, base0D, base0E, base0F, ... }@numbered:
   rec {
     numbered = [ base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F ];
-
-    inherit numbered;
     numberedHashtag = builtins.mapAttrs (_: v: "#" + v) numbered;
     numberedDec = builtins.mapAttrs (name: color: colorHex2Dec color) numbered;
 
