d32248351c260fba0a4943b90fb20d0e5411b8ba
Arseniy Chekanov <senya@chekanov.net>
2021-05-16 23:19:18 +0300

    Feat: Actually add NixOS module

---
+++ nixos-module.nix
@@ -0,0 +1,57 @@
+{ self, lib, pkgs, config, ... }:
+
+with lib;
+
+let
+  cfg = config.base16;
+
+  colorType = types.str;
+
+  schemeType = mkOption {
+    description = "A scheme";
+    type = types.submodule {
+      options = rec {
+        numbered = mkOption {
+          description =
+            "List of base16 colors in order 00, 01, ..., 0F in format of 'rrggbb'";
+          type = types.listOf colorType;
+        };
+
+        numberedHashtag = mkOption {
+          description =
+            "List of base16 colors in order 00, 01, ..., 0F in format of '#rrggbb'";
+          type = types.listOf colorType;
+        };
+
+        numberedDec = mkOption {
+          description =
+            "List of base16 colors in order 00, 01, ..., 0F in format of 'r,g,b', e.g. r is decimal form of '#rr'";
+          type = types.listOf colorType;
+        };
+
+        named = mkOption {
+          description =
+            "Same as 'numbered', but as an attrset, where keys are semantic color names, e.g. 'blue' refers to base0D";
+          type = types.attrsOf colorType;
+        };
+
+        namedHashtag = mkOption {
+          description =
+            "Same as 'numberedHashtag', but as an attrset";
+          type = types.attrsOf colorType;
+        };
+
+        namedDec = mkOption {
+          description =
+            "Same as 'numberedDec', but as an attrset";
+          type = types.attrsOf colorType;
+        };
+      };
+    };
+  }
+in {
+  options.base16.schemes = {
+    description = "Attribute set of schemes";
+    type = types.attrsOf schemeType;
+  }
+}
